301eca70948d9a86bfdd4561f4279e80aa3748b4
Brian Jing <brian.jing@au.experian.com>
2015-08-13 17:31:47 +1000

    End of chapter 12

--- tango_with_jango_project/settings.py
+++ tango_with_jango_project/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rango',
+    'registration',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -97,7 +98,18 @@ STATICFILES_DIRS = (
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # Absolute path to the media directory
 
-LOGIN_URL = '/rango/login/'
+# Registration related variables
+# If True, users can register
+REGISTRATION_OPEN = True
+# One-week activation window; you may, of course, use a different value.
+ACCOUNT_ACTIVATION_DAYS = 7
+# If True, the user will be automatically logged in.
+REGISTRATION_AUTO_LOGIN = True
+# The page you want users to arrive at after they successful log in
+LOGIN_REDIRECT_URL = '/rango/'
+# The page users are directed to if they are not logged in,  and are trying to access pages
+# requiring authenticatio
+LOGIN_URL = '/accounts/login/'
 
 # Session related variables
 SESSION_EXPIRE_AT_BROWSER_CLOSE = False
